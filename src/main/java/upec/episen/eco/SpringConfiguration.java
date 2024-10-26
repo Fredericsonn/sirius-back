9683322119e2031915d37bfdda4eea152a7ea09b
Ayoub <ayou2017boudra@gmail.com>
2024-10-26 18:16:02 +0200

    up

---
+++ src/main/java/upec/episen/eco/SpringConfiguration.java
@@ -0,0 +1,14 @@
+package upec.episen.eco;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootConfiguration
+public class SpringConfiguration implements WebMvcConfigurer {
+    @Override   
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*");
+    }
+}
