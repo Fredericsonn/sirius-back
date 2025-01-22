883172d94d5e00a73ac8f8764d33130778a18886
GNABARR <162433630+GNABARR@users.noreply.github.com>
2025-01-22 16:09:22 +0100

    back-end work with data.sql file, back is ready

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO comsumption(id,name) VALUES (0,'justtest');
+INSERT INTO comsumption(id,name) VALUES (1,'justtest');
+INSERT INTO consumption_item (id, consumption_id, carbon_footprint, quatity) VALUES (1, 1, 150.0, 3);
+
+
 
