75ba8ef351b01ee7fcae523274c5dfc8530d30d5
GNABARR <162433630+GNABARR@users.noreply.github.com>
2025-01-22 18:20:58 +0100

    back-end R1 final version

--- src/main/java/upec/episen/eco/EcoApplication.java
+++ src/main/java/upec/episen/eco/EcoApplication.java
@@ -1,7 +1,9 @@
 package upec.episen.eco;
 
+import com.sun.glass.ui.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import upec.episen.eco.service.consumption.ComsumptionService;
 
 @SpringBootApplication
@@ -10,8 +12,9 @@ public class EcoApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(EcoApplication.class, args);
-		//ComsumptionService c = new ComsumptionService();
-		//System.out.println(c.getOrderedItemsById(1L));
+		//ApplicationContext context = SpringApplication.run(Application.class, args);
+		//ComsumptionService consumptionService = context.getBean(ComsumptionService.class);
+		//System.out.println(consumptionService.getOrderedItemsById(1L));
 	}
 
 }
