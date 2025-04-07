2d0de2252d982537e0b802dc3e8aaf0368b48a37
Ayoub <ayou2017boudra@gmail.com>
2025-04-07 16:45:56 +0200

    third test passed

--- src/test/java/upec/episen/eco/EcoApplicationTests.java
+++ src/test/java/upec/episen/eco/EcoApplicationTests.java
@@ -2,9 +2,8 @@ package upec.episen.eco;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import upec.episen.eco.config.TestAppConfig;
 
-@SpringBootTest(classes = TestAppConfig.class)
+@SpringBootTest
 class EcoApplicationTests {
 
     @Test
