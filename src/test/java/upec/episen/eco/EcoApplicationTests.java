6fdf93dfb7ea287d3e91558632c327caba5c61b3
Ayoub <ayou2017boudra@gmail.com>
2025-04-07 16:29:42 +0200

    pswd encoder

--- src/test/java/upec/episen/eco/EcoApplicationTests.java
+++ src/test/java/upec/episen/eco/EcoApplicationTests.java
@@ -3,6 +3,9 @@ package upec.episen.eco;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import upec.episen.eco.controllers.User.UserController;
+import upec.episen.eco.EcoApplication;
+
 @SpringBootTest(
     classes = EcoApplication.class,
     exclude = UserController.class
